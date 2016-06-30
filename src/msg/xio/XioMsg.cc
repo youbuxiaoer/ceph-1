trivikram29/ceph

blkin: add traces to XioMessenger
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/msg/xio/XioMsg.cc
@@ -27,6 +27,7 @@ int XioDispatchHook::release_msgs()
   /* queue for release */
   xrsp = static_cast<XioRsp *>(rsp_pool.alloc(sizeof(XioRsp)));
   new (xrsp) XioRsp(xcon, this);
+  xrsp->trace = m->trace;
 
   /* merge with portal traffic */
   xcon->portal->enqueue_for_send(xcon, xrsp);

src/msg/xio/XioPortal.h
@@ -170,6 +170,7 @@ public:
 	xrsp->xcon->msg_release_fail(msg, code);
       msg = next_msg;
     }
+    xrsp->trace.event("xio_release_msg");
     xrsp->finalize(); /* unconditional finalize */
   }
 
@@ -274,7 +275,7 @@ public:
 		  requeue_all_xcon(xcon, q_iter, send_q);
 		  goto restart;
 		}
-
+		xs->trace.event("xio_send_msg");
 		msg = &xmsg->req_0.msg;
 		code = xio_send_msg(xcon->conn, msg);
 		/* header trace moved here to capture xio serial# */

youbuxiaoer/ceph-1

src/msg/xio/XioMsg.cc
@@ -27,6 +27,7 @@ int XioDispatchHook::release_msgs()
   /* queue for release */
   xcmp = static_cast<XioCompletion *>(rsp_pool.alloc(sizeof(XioCompletion)));
   new (xcmp) XioCompletion(xcon, this);
+  xcmp->trace = m->trace;
 
   /* merge with portal traffic */
   xcon->portal->enqueue(xcon, xcmp);

src/msg/xio/XioPortal.h
@@ -170,6 +170,7 @@ public:
 	xcmp->xcon->msg_release_fail(msg, code);
       msg = next_msg;
     }
+    xcmp->trace.event("xio_release_msg");
     xcmp->finalize(); /* unconditional finalize */
   }
 
@@ -273,6 +274,7 @@ public:
 		  goto restart;
 		}
 
+		xs->trace.event("xio_send_msg");
 		msg = xsend->get_xio_msg();
 		code = xio_send_msg(xcon->conn, msg);
 		/* header trace moved here to capture xio serial# */